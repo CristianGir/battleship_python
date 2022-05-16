bce80286e451b8b5936185f2b97af308bead7863
Miospi <miospi88@gmail.com>
2022-05-15 22:02:58 -0500

    Creación de la clase Board.

---
+++ model/board.py
@@ -0,0 +1,10 @@
+
+class Board:
+    def __init__(self, rows, cols, player, ships_list):
+        self.rows = rows
+        self.cols = cols
+        self.player = player
+        self.ships_list = ships_list
+
+    def validate_shoot(self):
+        pass
